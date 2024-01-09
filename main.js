5ff82f0e4cb3aed2359bc1dfb68da1dcceea2c0a
Tsung-Yu Chan <jkcharlie6679@gmail.com>
2024-01-09 17:52:25 +0800

    feat / add the discord bot
      - add the discord for pushing daily challenge

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import schedule from "node-schedule"
 import { getAuthToken, updateSheet, getSheetId, drawColor } from "./src/sheetApi.js";
 import { fetchDailyCodingChallenge } from "./src/leetcodeApi.js";
 import { logInit } from "./src/log.js"
+import { Client, GatewayIntentBits, EmbedBuilder} from "discord.js";
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS = "./credentials.json"
 process.env.TZ = "UTC"
@@ -10,17 +11,26 @@ process.env.TZ = "UTC"
 const spreadsheetId = process.env.GOOGLE_SPREADSHEET_ID;
 assert(spreadsheetId !== undefined, "Please provide the soreadsheet ID.")
 
+const discordBotToken = process.env.DISCORD_BOT_TOKEN;
+assert(discordBotToken !== undefined, "Please provide the discord bot token.")
+
+const dcChannelId = process.env.DISCORD_CHANNEL_ID;
+assert(dcChannelId !== undefined, "Please provide the discord channel ID.")
+
+
 const difficultyColor = {
   "Easy": "b7d7a8",
   "Medium": "ffe599",
   "Hard": "ea9999"
 }
 
-await logInit();
+const client = new Client({ intents: [GatewayIntentBits.Guilds]});
 
-schedule.scheduleJob("5 0 * * *", () => {
-  getAuthToken().then(async (authToken) => {
+logInit();
 
+getAuthToken().then(async (authToken) => {
+  await client.login(discordBotToken)
+  schedule.scheduleJob("5 0 * * *", async() => {
     const todayStr = new Date().toLocaleString({ timeZone: "UCT" });
     const today = new Date(todayStr);
     const discussDay = new Date;
@@ -38,8 +48,20 @@ schedule.scheduleJob("5 0 * * *", () => {
 
     const resource = { values: [[difficulty, "=HYPERLINK(\"" + url + "\", \"" + questionId + ". " + title + "\")"]] };
     await updateSheet(authToken, spreadsheetId, updateRange, resource);
-
     const colorRange = "A" + (day + 1) + ":A" + (day + 1);
     await drawColor(authToken, spreadsheetId, sheedId, colorRange, difficultyColor[difficulty]);
+
+    const channel = client.channels.cache.get(dcChannelId)
+    const exampleEmbed = new EmbedBuilder()
+	    .setColor(difficultyColor[difficulty])
+	    .setTitle("Daily Challenge! " + title)
+	    .setURL(url)
+	    .addFields(
+	    	{ name: 'Title', value: title },
+        { name: 'Difficulty', value: difficulty },
+	    )
+	    .setImage("https://random.imagecdn.app/1440/1024")
+    channel.send({ embeds: [exampleEmbed] })
   });
 });
+
